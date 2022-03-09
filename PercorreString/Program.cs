e813b29f77ec6abe1917cb5e6217e7fe92fdd9f7
mspappa <marco.duarte.santos@hotmail.com>
2022-03-09 23:13:32 +0000

    1 commit

---
+++ PercorreString/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace OlaMundo
+///
+{
+    class Program
+    ///
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("insira a string\n");
+            string s = Console.ReadLine(); 
+
+            Console.WriteLine("insira o caractere a omitir");
+            char c = Console.Read(); 
+
+            Convert.ToChar(s);
+
+            for (int i = 0; i < args.Length; i++) //
+            {
+                if (s[i] == c)
+                {
+                    s -= c;
+                }
+            }
+        }
+    }
+}
